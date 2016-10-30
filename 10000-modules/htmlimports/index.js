5a0584a282e6f95fdade32a587a64dc3b057ceae
pemrouz <p3pedram@hotmail.com>
2016-10-30 01:01:00 +0100

    feat: switch htmlimports to http2

--- 10000-modules/htmlimports/index.js
+++ 10000-modules/htmlimports/index.js
@@ -1,6 +1,10 @@
-var app = require('express')()
-  , static = require('serve-static')
+const fs = require('fs')
+    , app = require('express')()
+    , static = require('serve-static')
+    , options = {
+        key: fs.readFileSync('./server.key')
+      , cert: fs.readFileSync('./server.crt')
+      }
+    , server = require('spdy').createServer(options, app).listen(2000, d => console.log("listening 2000"))
 
-app
-  .use(static(__dirname))
-  .listen(2000, d => console.log("listening 2000"))
+app.use(static(__dirname))
